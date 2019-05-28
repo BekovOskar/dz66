19e0fece6450202b335cfa581c692a6d2aedcd48
Oskar <bek.offsll@gmail.com>
2019-05-28 17:35:03 +0600

    Server Commit

---
+++ src/test/java/com/bekov/dz66mnserver/Dz66mnserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bekov.dz66mnserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Dz66mnserverApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
